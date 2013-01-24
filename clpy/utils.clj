5fd91c7917c22eba3dea49997bad3f9c6dfa2bd8
Michał Zieliński <michal@zielinscy.org.pl>
2013-01-24 23:49:44 +0100

    add a few utilities

--- clpy/utils.clj
+++ clpy/utils.clj
@@ -3,12 +3,16 @@
 (defn fetch [coll key]
   (let [val (get coll key)]
     (if val val
-        (throw (Exception. (format "%s not found in %s" key coll))))))
+        (throw (Exception. (format "%s not found in %s" key (class coll)))))))
 
 (defn fetch-in [coll ks]
   (if (seq ks)
     (fetch-in (fetch coll (first ks)) (rest ks))
     coll))
+
+; update-in exists, so why not update?
+(defn update [coll k func]
+  (update-in coll [k] func))
 
 (defn get-symbol-name [sym]
   ; todo
@@ -20,3 +24,10 @@
 (defn hash-map-from-items [pairs]
   {:pre (every? #(= (count %) 2) pairs)}
   (apply hash-map (mapcat identity pairs)))
+
+(defn pop-n [stack n]
+  (let [[front stack] (loop [stack stack, n n, front nil]
+             (if (= n 0)
+               [front stack]
+               (recur (rest stack) (dec n) (cons (first stack) front))))]
+    [(reverse front) stack]))
